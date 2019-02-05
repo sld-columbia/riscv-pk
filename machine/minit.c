79e7c45194a963a8b4482ede367606b17a9d42ca
Michael Schaffner <schaffner@iis.ee.ethz.ch>
2019-02-05 11:14:18 +0100

    Add uart16750 parser, automatically parse baud/frequency settings from DTB.

--- machine/minit.c
+++ machine/minit.c
@@ -7,6 +7,7 @@
 #include "fdt.h"
 #include "uart.h"
 #include "uart16550.h"
+#include "uart16750.h"
 #include "uart_lite.h"
 #include "finisher.h"
 #include "disabled_hart_mask.h"
@@ -156,6 +157,7 @@ void init_first_hart(uintptr_t hartid, uintptr_t dtb)
   // Confirm console as early as possible
   query_uart(dtb);
   query_uart16550(dtb);
+  query_uart16750(dtb);
   query_uart_lite(dtb);
   query_htif(dtb);
   printm("bbl loader\r\n");
--- machine/uart16550.c
+++ machine/uart16550.c
@@ -4,7 +4,7 @@
 #include "uart16550.h"
 #include "fdt.h"
 
-volatile uint32_t* uart16550;
+volatile uint8_t* uart16550;
 
 #define UART_REG_QUEUE     0
 #define UART_REG_LINESTAT  5
@@ -29,6 +29,8 @@ struct uart16550_scan
 {
   int compat;
   uint64_t reg;
+  uint32_t freq;
+  uint32_t baud;
 };
 
 static void uart16550_open(const struct fdt_scan_node *node, void *extra)
@@ -40,8 +42,12 @@ static void uart16550_open(const struct fdt_scan_node *node, void *extra)
 static void uart16550_prop(const struct fdt_scan_prop *prop, void *extra)
 {
   struct uart16550_scan *scan = (struct uart16550_scan *)extra;
-  if (!strcmp(prop->name, "compatible") && !strcmp((const char*)prop->value, "ns16750")) {
+  if (!strcmp(prop->name, "compatible") && !strcmp((const char*)prop->value, "ns16550")) {
     scan->compat = 1;
+  } else if (!strcmp(prop->name, "clock-frequency")) {
+    fdt_get_value(prop->value, &scan->freq);
+  } else if (!strcmp(prop->name, "current-speed")) {
+    fdt_get_value(prop->value, &scan->baud);
   } else if (!strcmp(prop->name, "reg")) {
     fdt_get_address(prop->node->parent, prop->value, &scan->reg);
   }
@@ -53,11 +59,15 @@ static void uart16550_done(const struct fdt_scan_node *node, void *extra)
   if (!scan->compat || !scan->reg || uart16550) return;
 
   uart16550 = (void*)(uintptr_t)scan->reg;
+
+  // calculate divisor
+  uint32_t divisor = scan->freq / (scan->baud << 4);
+
   // http://wiki.osdev.org/Serial_Ports
   uart16550[1] = 0x00;    // Disable all interrupts
   uart16550[3] = 0x80;    // Enable DLAB (set baud rate divisor)
-  uart16550[0] = 0x1B;    // Set divisor to 3 (lo byte) 38400 baud
-  uart16550[1] = 0x00;    //                  (hi byte)
+  uart16550[0] = divisor      & 0xFF;    // divisor (lo byte) 
+  uart16550[1] = (divisor>>8) & 0xFF;    // divisor (hi byte)
   uart16550[3] = 0x03;    // 8 bits, no parity, one stop bit
   uart16550[2] = 0xC7;    // Enable FIFO, clear them, with 14-byte threshold
 }
@@ -66,6 +76,10 @@ void query_uart16550(uintptr_t fdt)
 {
   struct fdt_cb cb;
   struct uart16550_scan scan;
+
+  // default
+  scan.freq = 50000000;
+  scan.baud = 115200;
 
   memset(&cb, 0, sizeof(cb));
   cb.open = uart16550_open;
